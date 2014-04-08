8c6c3e691ed0bfbe672513b7558a7e441ddc032b
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-04-08 19:32:09 -0400

    Attempt to use MSBuild 4.0 and not 12.0

--- build.cmd
+++ build.cmd
@@ -2,8 +2,6 @@
 setlocal
 set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0
 set PATH=%PATH%;%ProgramFiles%\Microsoft SDKs\F#\3.1\Framework\v4.0
-set PATH=%PATH%;%ProgramFiles%\MSBuild\12.0\Bin
-set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\12.0\Bin
 set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 tools\NuGet.exe install DotNetZip -o packages -excludeVersion
-MSBuild.exe src/FsNuGet/FsNuGet.fsproj /p:Configuration=Release
+MSBuild.exe src/FsNuGet/FsNuGet.fsproj /p:Configuration=Release /p:VisualStudioVersion=12.0
